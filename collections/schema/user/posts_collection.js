0cf142220247bbe9792173434210f3aa5385bc1c
Louis Lainé <louis.laine7@gmail.com>
2016-01-12 17:59:56 +0100

    Ajout du schema pour les articles

---
+++ collections/schema/user/posts_collection.js
@@ -0,0 +1,16 @@
+UserPosts = new Mongo.Collection('user-posts');
+UserPosts.schema = new SimpleSchema({
+  _id: { type: String },
+  post_attributes: {
+    type: Object,
+    // Ne contient qu'un seul objet de ce type
+    minCount: 1,
+    maxCount: 1
+  },
+  // Attributes de l'object posts
+  'post_attributes.$.title': { type : String },
+  'post_attributes.$.body': { type : String },
+  // TODO Pour plus tard : rajouter les comments
+  'post_attributes.$.create_at': { type: Date },
+  user_id: { type: Number }
+});
